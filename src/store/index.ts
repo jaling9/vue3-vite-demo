f87d41f2cb92f770ecbbb1d4c0ca9195a9a58541
manjialindeMacBook-Pro <mjl691792623@gmail.com>
2023-04-12 09:55:27 +0800

    Update index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,26 @@
-
 import { defineStore } from 'pinia'
+import { Names } from './text' //抽离出去的名字最终还是需要引入回来store文件进行使用的
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
+export const useTestStore = defineStore(Names.Test, {
+  state: () => {
+    return {
+      current: '晚上好',
+      name: '小满',
+      age: 18
+    }
+  },
+  getters: {
+    // getters are like computed properties
+    // they are cached and only re-evaluated when their dependencies change
+    // they are also reactive
+    // they are exposed on the store.$ getters property
+  },
+  actions: {
+    // actions are like methods
+    // they are not cached
+    // they are not reactive
+    // they are exposed on the store.$ actions property
   }
-
-  return { count, doubleCount, increment }
 })
+//通过源码我们可以看到这里是：
+// state?:()=> S;//意思就是一个箭头函数，可选填入
