3ed84231cf348771d7dbb22433c827787d9c3b03
manjialindeMacBook-Pro <mjl691792623@gmail.com>
2023-04-12 09:55:22 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
-  <div></div>
+  <div>hello pinia!{{ Test.current }}</div>
 </template>
 
 <script setup lang="ts">
 import { ref, reactive } from 'vue'
+import { useTestStore } from './store'
+const Test = useTestStore()
 </script>
 <style scope></style>
