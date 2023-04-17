63dfd1de7b5d639f63630c33d1b3113285a698b7
jaling9 <mjl691792623@gmail.com>
2023-04-17 17:25:30 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -1,192 +1,33 @@
 <template>
-  <div
-    :style="{
-      backgroundImage: `url(${bg}) `
-    }"
-    class="box"
-  >
-    <div class="box-left"></div>
-    <div class="box-center" id="chinaMap"></div>
-    <div class="box-right"></div>
-  </div>
+  <h1>父组件</h1>
+  <br />
+  <propsComponent
+    ref="dataFromDefineExpose"
+    @on-click="getName"
+    :arr="hah"
+    :title="name"
+  ></propsComponent>
 </template>
 
 <script setup lang="ts">
-import { ref, reactive } from 'vue'
-import bg from './assets/images/bg.png'
-// import { useStore } from './stores'
-import data from './server/data.json'
-import { onMounted } from 'vue'
-import * as echarts from 'echarts' // echarts 5.0.2引入方式
-import './server/china.js' // 引入地图数据
-onMounted(() => {
-  const charts = echarts.init(document.querySelector('#chinaMap') as HTMLElement)
-  var data = [
-    {
-      name: '内蒙古',
-      itemStyle: {
-        areaColor: '#56b1da'
-      },
-      value: [110.3467, 41.4899]
-    }
-  ]
-  charts.setOption({
-    backgroundColor: 'black',
-    geo: {
-      map: 'china',
-      aspectScale: 0.8,
-      layoutCenter: ['50%', '50%'],
-      layoutSize: '120%',
-      itemStyle: {
-        normal: {
-          areaColor: {
-            type: 'linear-gradient',
-            x: 0,
-            y: 1200,
-            x2: 1000,
-            y2: 0,
-            colorStops: [
-              {
-                offset: 0,
-                color: '#152E6E' // 0% 处的颜色
-              },
-              {
-                offset: 1,
-                color: '#0673AD' // 50% 处的颜色
-              }
-            ],
-            global: true // 缺省为 false
-          },
-          shadowColor: '#0f5d9d',
-          shadowOffsetX: 0,
-          shadowOffsetY: 15,
-          opacity: 0.5
-        },
-        emphasis: {
-          areaColor: '#0f5d9d'
-        }
-      },
+import propsComponent from './components/propsComponent.vue'
+import { ref, reactive, onMounted, onBeforeUnmount } from 'vue'
 
-      regions: [
-        {
-          name: '南海诸岛',
-          itemStyle: {
-            areaColor: 'rgba(0, 10, 52, 1)',
-            borderColor: 'rgba(0, 10, 52, 1)',
-            normal: {
-              opacity: 0,
-              label: {
-                show: false,
-                color: '#009cc9'
-              }
-            }
-          },
-          label: {
-            show: false,
-            color: '#FFFFFF',
-            fontSize: 12
-          }
-        }
-      ]
-    },
-    series: [
-      {
-        type: 'map',
-        selectedMode: 'multiple',
-        mapType: 'china',
-        aspectScale: 0.8,
-        layoutCenter: ['50%', '50%'], //地图位置
-        layoutSize: '120%',
-        zoom: 1, //当前视角的缩放比例
-        // roam: true, //是否开启平游或缩放
-        scaleLimit: {
-          //滚轮缩放的极限控制
-          min: 1,
-          max: 2
-        },
-        label: {
-          show: false,
-          color: '#FFFFFF',
-          fontSize: 16
-        },
-        itemStyle: {
-          normal: {
-            areaColor: '#0c3653',
-            borderColor: '#1cccff',
-            borderWidth: 1.8
-          },
-          emphasis: {
-            areaColor: '#56b1da',
-            label: {
-              show: false,
-              color: '#fff'
-            }
-          }
-        },
-        data: data
-      },
-      {
-        name: 'Top 5',
-        type: 'scatter',
-        coordinateSystem: 'geo',
-        //   symbol: 'image://http://ssq168.shupf.cn/data/biaoji.png',
-        // symbolSize: [30,120],
-        // symbolOffset:[0, '-40%'] ,
-        label: {
-          normal: {
-            show: false
-          }
-        },
-        itemStyle: {
-          normal: {
-            color: '#D8BC37' //标志颜色
-          }
-        },
-        data: data,
-        showEffectOn: 'render',
-        rippleEffect: {
-          brushType: 'stroke'
-        },
-        hoverAnimation: true,
-        zlevel: 1
-      }
-    ]
-  })
-})
+const dataFromDefineExpose = ref<InstanceType<typeof propsComponent>>()
 
-console.log(data)
-// console.log(useStore)
+//然后就可以读取到了
+
+console.log(dataFromDefineExpose.value?.myName)
+console.log(dataFromDefineExpose.value?.open())
+//父组件能通过defineExpose就能够读取到子组件的属性，或者调子组件的方法
+
+// propsComponent1.value.open()
+// console.log(propsComponent1.value.myName)
+
+const name = 'xiaoman'
+const hah = [1, 2, 3, 4, 5]
+const getName = (val: string) => {
+  console.log(val)
+}
 </script>
-<style lang="less">
-html,
-body,
-#app {
-  height: 100%;
-  margin: 0;
-  padding: 0;
-}
-
-.box {
-  display: flex;
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: 100% 100%; /* 或者 cover  contain*/
-  /* ... */
-  // flex-direction: row;
-  // justify-content: last baseline;
-  // align-items: stretch;
-  height: 100%;
-  .box-left {
-    width: 400px;
-    // background-color: #f00;
-  }
-  .box-center {
-    flex: 1;
-    // background-color: #0f0;
-  }
-  .box-right {
-    width: 400px;
-    // background-color: #00f;
-  }
-}
-</style>
+<style style="css"></style>
--- src/components/propsComponent.vue
+++ src/components/propsComponent.vue
@@ -4,16 +4,14 @@
   <h3>值：{{ arr }}</h3>
   <p-Button @click="send">给父组件传值</p-Button>
 </template>
-
 <script setup lang="ts">
 import { ref, reactive } from 'vue'
-
+import { defineProps, defineEmits, defineExpose, withDefaults } from 'vue'
 // defineProps作用是接受父组件传递的值
 // withDefaults作用是给子组件传递默认值
-
 withDefaults(defineProps<{ title: string; arr: number[] }>(), {
   title: '这是一个子组件',
-  arr: [1]
+  arr: [1, 2, 3]
 })
 // const props = defineProps({
 //   title: {
@@ -25,14 +23,13 @@ withDefaults(defineProps<{ title: string; arr: number[] }>(), {
 
 // 给父组件传值使用defineEmits
 const emit = defineEmits<{ (e: 'on-click', val: string): void }>()
-
 const send = () => {
-  emit('on-click', '我是子组件传递的值')
+  emit('on-click', '我是子组件传递的事件')
 }
 
 // 给父组件暴露一些方法使用defineExpose
 defineExpose({
-  myName: 'xiaoman',
+  myName: '我是子组件暴露的属性',
   open() {
     console.log('我是子组件暴露的方法')
   }
