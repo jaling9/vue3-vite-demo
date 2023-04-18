82d5dc7f786617bf5b75a1328f9f7f3748ee79b3
jaling9 <mjl691792623@gmail.com>
2023-04-18 16:43:26 +0800

    Update App CSS样式穿透deep.vue

--- src/App CSS样式穿透deep.vue
+++ src/App CSS样式穿透deep.vue
@@ -1,11 +1,3 @@
-<!--
- * @Author: blurryFace9 50992587+jaling9@users.noreply.github.com
- * @Date: 2023-03-23 20:28:01
- * @LastEditors: blurryFace9 50992587+jaling9@users.noreply.github.com
- * @LastEditTime: 2023-04-11 19:56:19
- * @FilePath: \vue3-vite-demo\src\App.vue
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
--->
 <template>
   <main>
     <el-input placeholder="测试代码" class="ipt"></el-input>
